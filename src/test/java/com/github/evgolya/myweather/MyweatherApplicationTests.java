5c25d373e6e625afc9d557a62a198053a9f54c7e
kolegran <kolegran@gmail.com>
2021-07-06 02:19:22 +0300

    Initial commit

---
+++ src/test/java/com/github/evgolya/myweather/MyweatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.evgolya.myweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyweatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
