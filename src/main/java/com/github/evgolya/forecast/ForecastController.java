075e0412286a3059bc048322ed02f6537e45dc04
kolegran <kolegran@gmail.com>
2021-08-12 02:38:42 +0300

    Changed method from Get to Post, added 'consumes' argument, added 'Access-Control-Allow-Origin' header

--- src/main/java/com/github/evgolya/forecast/ForecastController.java
+++ src/main/java/com/github/evgolya/forecast/ForecastController.java
@@ -2,8 +2,11 @@ package com.github.evgolya.forecast;
 
 import com.github.evgolya.forecast.apiclient.ForecastApiClient;
 import com.github.evgolya.forecast.dto.LocalityCoordinates;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,13 +24,19 @@ public class ForecastController {
         return "Server is available";
     }
 
-    @GetMapping("/current-weather")
-    public String getCurrentWeather(@RequestBody LocalityCoordinates localityCoordinates) {
-        return forecastApiClient.getCurrentWeatherByCoordinates(localityCoordinates.getLatitude(), localityCoordinates.getLongitude());
+    @PostMapping(path = "/current-weather", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getCurrentWeather(@RequestBody LocalityCoordinates localityCoordinates) {
+        return ResponseEntity
+            .ok()
+            .header("Access-Control-Allow-Origin", "*")
+            .body(forecastApiClient.getCurrentWeatherByCoordinates(localityCoordinates.getLatitude(), localityCoordinates.getLongitude()));
     }
 
-    @GetMapping("/forecast/{days}")
-    public String getForecastForMultipleDays(@PathVariable Integer days, @RequestBody LocalityCoordinates localityCoordinates) {
-        return forecastApiClient.getForecastByCoordinates(days, localityCoordinates.getLatitude(), localityCoordinates.getLongitude());
+    @PostMapping(path = "/forecast/{days}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> getForecastForMultipleDays(@PathVariable Integer days, @RequestBody LocalityCoordinates localityCoordinates) {
+        return ResponseEntity
+            .ok()
+            .header("Access-Control-Allow-Origin", "*")
+            .body(forecastApiClient.getForecastByCoordinates(days, localityCoordinates.getLatitude(), localityCoordinates.getLongitude()));
     }
 }
