9e9b397ca649e9e749af33feffcf199a4126ceae
kolegran <kolegran@gmail.com>
2021-09-26 21:47:00 +0300

    Removed redundant endpoints

--- src/main/java/com/github/evgolya/weatherapi/forecast/ForecastController.java
+++ src/main/java/com/github/evgolya/weatherapi/forecast/ForecastController.java
@@ -1,11 +1,8 @@
 package com.github.evgolya.weatherapi.forecast;
 
 import com.github.evgolya.geolocationapi.address.SearchedLocality;
-import com.github.evgolya.weatherapi.LocalityCoordinates;
-import com.github.evgolya.weatherapi.forecast.currentweatherdto.CurrentWeatherDto;
 import com.github.evgolya.weatherapi.forecast.currentweatherdto.ExtendedCurrentWeatherDto;
 import com.github.evgolya.weatherapi.forecast.fullforecastdto.ExtendedFullForecastDto;
-import com.github.evgolya.weatherapi.forecast.fullforecastdto.FullForecastDto;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 // TODO: set header to a response instead duplicating (https://www.baeldung.com/spring-response-header#3-adding-a-header-for-all-responses)
 @RestController
@@ -30,27 +29,9 @@ public class ForecastController {
         return "Server is available";
     }
 
-    @PostMapping(path = "/current-weather", consumes = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public ResponseEntity<CurrentWeatherDto> getCurrentWeather(@RequestBody LocalityCoordinates localityCoordinates) {
-        return ResponseEntity
-            .ok()
-            .header("Access-Control-Allow-Origin", "*")
-            .body(forecastDataProvider.getCurrentWeatherByCoordinates(localityCoordinates.getLatitude(), localityCoordinates.getLongitude()));
-    }
-
-    @PostMapping(path = "/forecast/{days}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseBody
-    public ResponseEntity<FullForecastDto> getForecastForMultipleDays(@PathVariable Integer days, @RequestBody LocalityCoordinates localityCoordinates) {
-        return ResponseEntity
-            .ok()
-            .header("Access-Control-Allow-Origin", "*")
-            .body(forecastDataProvider.getForecastByCoordinates(days, localityCoordinates.getLatitude(), localityCoordinates.getLongitude()));
-    }
-
     @PostMapping(path = "/current-weather/locality", consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public ResponseEntity<ExtendedCurrentWeatherDto> getCurrentWeatherForLocality(@RequestBody SearchedLocality searchedLocality) {
+    public ResponseEntity<ExtendedCurrentWeatherDto> getCurrentWeatherForLocality(@RequestBody SearchedLocality searchedLocality, HttpServletRequest httpServletRequest) {
         return ResponseEntity
             .ok()
             .header("Access-Control-Allow-Origin", "*")
